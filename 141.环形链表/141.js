dd809a9f4a6a6749b68afcf9692ebf2f29315840
guohaozhong <1033682873@qq.com>
2021-07-15 11:51:53 +0800

    update

--- 141.环形链表/141.js
+++ 141.环形链表/141.js
@@ -17,16 +17,17 @@
  * @return {boolean}
  */
  var hasCycle = function(head) {
-    if(!head){
+    if(!head || !head.next){
         return false
     }
-    let fast=head
-    let slow=head
+    let fast=head //快指针-移动2位
+    let slow=head //慢指针-移动1位
     
     while(fast && fast.next){
         fast=fast.next.next
         slow=slow.next
         if(fast==slow){
+            //快慢指针相等--环形链表
             return true
         }
     }
